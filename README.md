74a44c64117791847289a4255a6f8fda4ea31888
legoheld <dreckig@sandkasten.ch>
2015-04-27 14:18:33 +0200

    added documentation. finished first version that compiles typescript with sourcemaps and minified version.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # lernetz-typescript-gulp-task
-This node module that wraps several gulp plugins into a reusable glup task with options  
+This node module wraps several gulp plugins executions into one reusable glup task.
+The main goal of the task is to compile a set of typescript files into a single js file.
+It automatically creates a minified version and injects the sourcemaps.
+
+
+## Usage
+The following example will compile all the files under the folder `typescript` into to output folder `public`.
+It creates the files:
+* demo.js - the uncompressed js source including the sourcemap
+* demo.min.js - the minified js file
+* demo.d.ts - the declaration file to be used in other projects
+
+```javascript
+var gulp = require('gulp');
+var tsTask = require( 'lernetz-typescript-gulp-task' );
+
+gulp.task( 'typescript', tsTask( { bundle:'demo', out:'public', src:'typescript/**/*.ts' } );
+```
+
+## Options
+The task accepts an parameter object with the following attributes:
+```javascript
+{
+    bundle: 'name', // the name of the files to create
+    src: 'src/**.ts', // the source parameter for the gulp.src( .. )
+    dest: 'public', // the destination used in gulp.dest( .. )
+    ts: { .. }, // the settings for the gulp-typescript plugin: https://www.npmjs.com/package/gulp-typescript#options
+}
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var merge = require('merge');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require( 'gulp-typescript');
+var uglify = require('gulp-uglify');
+var mergeStream = require('merge2');
+var rename = require("gulp-rename");
+
+
+module.exports = function( options ) {
+	
+	var bundle = ( options.bundle ) ? options.bundle : 'main';
+	
+	var defaults = {
+		ts: {
+			out: bundle + '.js',
+			declarationFiles: true
+		},
+		src:'typescript/**/*.ts',
+		dest:'public/js'
+	}
+	
+	// merge options with default values
+	options = merge( defaults, options );
+	
+	// create typescript project for incemental compile
+	var tsProject = ts.createProject( options.ts );
+	
+	// return task function
+	return function() {
+		
+		// do gulp stuff
+		var result = gulp.src( options.src )
+		.pipe( sourcemaps.init() )
+		.pipe( ts( tsProject ) );
+
+		return mergeStream( [
+		                     
+		    // write out declaration file
+			result.dts.pipe( gulp.dest( options.dest ) ),
+			
+			// write sourcemaps
+			result.js.pipe( sourcemaps.write() )
+			
+			// write original file
+			.pipe( gulp.dest( options.dest ) )
+			
+			// create minified version
+			.pipe( uglify() )
+			.pipe( rename( bundle + '.min.js' ) )
+			.pipe( gulp.dest( options.dest ) )
+			
+		]);
+	}
+}
