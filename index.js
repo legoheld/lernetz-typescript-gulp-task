5de645e5753b171d4cc43a975e9a434132f1fa18
legoheld <dreckig@sandkasten.ch>
2016-01-27 16:50:17 +0100

    switched to browserify.

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
+'use strict';
+
+var watchify = require('watchify');
+var browserify = require('browserify');
 var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var tsify = require('tsify');
 var merge = require('merge');
-var sourcemaps = require('gulp-sourcemaps');
-var ts = require( 'gulp-typescript');
 var uglify = require('gulp-uglify');
-var mergeStream = require('merge2');
 var rename = require("gulp-rename");
+var streamify = require('gulp-streamify');
 
 
-module.exports = function( options ) {
-
-	
-	var bundle = ( options.bundle ) ? options.bundle : 'main';
-	
-	var defaults = {
-		ts: {
-			out: bundle + '.js',
-			declaration: true,
-			target: 'ES5'
-		},
-		src:'**/*.ts',
-		dest:'js'
+function invoke( options ) {
+    
+    var defaults = {
+        bundle:'main',
+        dest:'public/out',
+		src: 'src/main.ts',
+        watch: true,
+        browserify: {
+            debug: true,
+            cache: {},
+            packageCache: {}
+        }
 	}
-	
+    
 	// merge options with default values
 	options = merge.recursive( defaults, options );
-	
-	// create typescript project for incemental compile
-	var tsProject = ts.createProject( options.ts );
-	
-	// return task function
-	return function() {
-		
-		// do gulp stuff
-		var result = gulp.src( options.src )
-		.pipe( sourcemaps.init() )
-		.pipe( ts( tsProject ) );
 
-		return mergeStream( [
-		                     
-		    // write out declaration file
-			result.dts.pipe( gulp.dest( options.dest ) ),
-			
-			// write sourcemaps
-			result.js.pipe( sourcemaps.write() )
-			
-			// write original file
-			.pipe( gulp.dest( options.dest ) )
-			
-			// create minified version
-			.pipe( uglify() )
-			.pipe( rename( bundle + '.min.js' ) )
-			.pipe( gulp.dest( options.dest ) )
-			
-		]);
-	}
+    // instantiate browserify
+    var bundler = browserify( options.browserify )
+    .add( options.src )
+    .plugin( tsify );
+    
+    // bundle function for reference
+    function bundle() {
+        return bundler.bundle()
+        .on( 'error', gutil.log.bind( gutil, 'Browserify Error' ) )
+		.pipe( source( options.bundle + '.js' ) )
+		.pipe( gulp.dest( options.dest ) )
+        .pipe( streamify( uglify() ) )
+        .pipe( rename( options.bundle + '.min.js' ) )
+        .pipe( gulp.dest( options.dest ) );
+    }
+    
+    if( options.watch ) {
+        bundler.plugin( watchify );
+        bundler.on( 'log', gutil.log); // output build logs to terminal
+        bundler.on( 'update', bundle );
+    }
+    
+    return bundle;
 }
+
+module.exports = invoke;
