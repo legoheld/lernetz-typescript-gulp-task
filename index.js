39c6b97d7dbf517627bfdcfd88f0aa248c5b0719
legoheld <michael.heimann@lernetz.ch>
2018-01-10 10:37:26 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 var gulp = require('gulp');
 var merge = require('merge');
 var sourcemaps = require('gulp-sourcemaps');
+var addsrc = require('gulp-add-src');
+var concat = require('gulp-concat');
 var rename = require("gulp-rename");
 var minify = require('gulp-minify');
 var rollup = require('gulp-better-rollup');
 var resolve = require( 'rollup-plugin-node-resolve' );
 var commonjs = require( 'rollup-plugin-commonjs' );
-var typescript = require( '@alexlur/rollup-plugin-typescript' );
+var typescript = require( 'rollup-plugin-typescript2' );
 
 
 
@@ -17,33 +19,45 @@ function invoke( options ) {
     var defaults = {
         bundle:'main',
         dest:'public/out',
-        globalName: 'app',
 		src: './src/main.ts',
         minify: { 
-            ext: { min:'.min.js' }
+            ext: { src:'.js', min:'.min.js' }
         },
         rollup: {
             format: 'iife',
             plugins: [
-				typescript(),
+				typescript( { check: false } ),
 				resolve( { jsnext: true, main: true, browser:true } ),
 				commonjs()
-			]
-        }
+			],
+            globals: {}
+        },
+        externals:[]
 	}
     
 	// merge options with default values
 	options = merge.recursive( defaults, options );
 
+    // reuse bundle name for globalName if not set
+    if( !options.globalName ) options.globalName = options.bundle;
+
     // inject the module name
     options.rollup.moduleName = options.globalName;
-    
+
+    // setup externals
+    var sources = options.externals.map( function( external ) { return external.source } );
+    options.rollup.external = options.externals.map( function( external ) { return external.module } );
+    options.externals.forEach( function( external ) {
+        options.rollup.globals[ external.module ] = external.global;
+    });
+
     // bundle function for reference
     function bundle() {
         return gulp.src( options.src )
 		.pipe( sourcemaps.init() )
 		.pipe( rollup( options.rollup, options.rollup.format ) )
-		.pipe( rename( options.bundle + '.js' ) )
+		.pipe( concat( options.bundle + '.js' ) )
+        .pipe( addsrc.prepend( sources ) )
 		.pipe( sourcemaps.write( '' ) )
 		.pipe( minify( options.minify ) )
         .pipe( gulp.dest( options.dest ) );
@@ -53,14 +67,5 @@ function invoke( options ) {
 }
 
 
-/**
- * Turns the string, string array with the expose name into a { file:.., expose:.. } object array
- */
-function mapRequire( src ) {
-	var parts = src.split( ':' );
-	var obj = { file:parts[0] };
-	if( parts.length > 1 ) obj.expose = parts[1];
-	return obj;
-}
 
 module.exports = invoke;
